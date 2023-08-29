608acf58f9754e688b0fbb40c5a45bed3303bfa0
Dario Walter Carrizo <108595018+dwc1970@users.noreply.github.com>
2023-08-29 17:33:14 -0300

    E- comerce
    Se actualiza la leccion 1 de la primer semana con la leccion 2 
    
    *** ACTUALIZACION ***

--- JAVASCRIPT 4/LECCION_1/ecomerce/cliente/js/index.js
+++ JAVASCRIPT 4/LECCION_1/ecomerce/cliente/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; // Este es nuestro carrito, un array vacio
 
 productos.forEach((producto) => {
     const content = document.createElement("div");
@@ -9,3 +10,21 @@ productos.forEach((producto) => {
     `;
     shopContent.append(content);
 });
+
+const buyButton = document.createElement("button");
+buyButton.innerText = "comprar";
+
+content.append(buyButton);
+
+buyButton.append(buyButton);
+
+buyButton.addEventListener("click", () => {
+    creat.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: product.quanty,
+        img : product.img,
+    })
+    console.log(cart)
+})
